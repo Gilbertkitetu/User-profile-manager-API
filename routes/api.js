8b3dc2e385a5997b33bdc2621cf48851bd5d236b
Gilbert kitetu <gilbertkitetu2@gmail.com>
2021-04-10 12:26:29 +0300

    Update , delete and get users

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,10 @@ const router = express.Router();
 
 //get 
 router.get('/user', function(req, res, next) {
-    res.send({ type: 'GET'});
+    //res.send({ type: 'GET'});
+    User.find({}).then(function(users){
+        res.send(users);
+    });
 });
 
 
@@ -22,6 +25,21 @@ router.post('/user', function(req, res, next){
 
 });
 
+//update a user 
+router.put('/user/:id', function(req, res, next){
+User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function(){
+    User.findOne({ _id: req.params.id }).then(function(user){
+        res.send(user);
+    });
+});
+});
 
+
+//delete a user
+router.delete('/user/:id', function(req, res, next) {
+    User.findByIdAndRemove({ _id: req.params.id }).then(function(user){
+        res.send(user);
+    });
+});
 
 module.exports = router;
